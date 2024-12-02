7e98de58b2fe234e70564dc5f26df24e64ca565b
rehansyed89 <rehansyed89@gmail.com>
2024-12-02 20:56:58 +0100

    otp modal in progress

---
+++ src/components/OTPModal.tsx
@@ -0,0 +1,116 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { REGEXP_ONLY_DIGITS } from "input-otp";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const OtpModal = ({
+  accountId,
+  email,
+}: {
+  accountId: string;
+  email: string;
+}) => {
+  console.log({ accountId, email });
+  const [isOpen, setIsOpen] = useState(true);
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const submitForm = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      // @Todo :  Call the API to verify OTP
+    } catch {
+      console.error("Could not verify OTP", email);
+    }
+    setIsLoading(false);
+  };
+
+  const handleResendOTP = () => {
+    // @Todo : Call the API to resend the OTP
+  };
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader className="relative flex justify-center">
+          <AlertDialogTitle className="h2 text-center">
+            Enter Your OTP
+            <Image
+              src="/assets/icons/close-dark.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => setIsOpen(false)}
+              className="otp-close-button"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription className="subtitle-2 text-center text-light-100">
+            We&apos;ve sent a code to
+            <span className="pl-1 text-brand">{email}</span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <InputOTP maxLength={6} value={password} onChange={setPassword}>
+          <InputOTPGroup className="shad-otp">
+            <InputOTPSlot index={0} className="shad-otp-slot" />
+            <InputOTPSlot index={1} className="shad-otp-slot" />
+            <InputOTPSlot index={2} className="shad-otp-slot" />
+            <InputOTPSlot index={3} className="shad-otp-slot" />
+            <InputOTPSlot index={4} className="shad-otp-slot" />
+            <InputOTPSlot index={5} className="shad-otp-slot" />
+          </InputOTPGroup>
+        </InputOTP>
+        <AlertDialogFooter>
+          <div className="flex w-full flex-col gap-4">
+            <AlertDialogAction
+              className="shad-submit-btn h-12"
+              type="button"
+              onClick={submitForm}
+            >
+              Submit
+              {isLoading && (
+                <Image
+                  src="/assets/icons/loader.svg"
+                  alt="loader"
+                  width={24}
+                  height={24}
+                  className="ml-2 animate-spin"
+                />
+              )}
+            </AlertDialogAction>
+            <div className="subtitle-2 mt-2 text-center text-light-100">
+              Didn&apos;t get a code?
+              <Button
+                type="button"
+                variant="link"
+                className="pl-1 text-brand"
+                onClick={handleResendOTP}
+              >
+                Click to resend
+              </Button>
+            </div>
+          </div>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default OtpModal;
---
+++ src/lib/actions/user.actions.ts
@@ -0,0 +1,65 @@
+"use server";
+
+import { createAdminClient } from "@/lib/appwrite";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { ID, Query } from "node-appwrite";
+import { parseStringify } from "@/lib/helpers";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])],
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+
+  if (!accountId) throw new Error("Account not found");
+
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.usersCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        avatar:
+          "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg",
+        accountId,
+      },
+    );
+  }
+
+  return parseStringify({ accountId });
+};
