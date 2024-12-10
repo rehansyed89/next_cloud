d6d03e8c4377c4f245caea1cd1546f4cd4d8f29c
rehansyed89 <rehansyed89@gmail.com>
2024-12-10 21:10:14 +0100

    Dynamic file route done

--- src/app/(root)/[type]/page.tsx
+++ src/app/(root)/[type]/page.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { getFileTypesParams } from "@/lib/utils";
 import Sort from "@/components/Sort";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ params, searchParams }: SearchParamProps) => {
   const type = (await params)?.type as string;
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
   const types = getFileTypesParams(type) as FileType[];
 
+  const files = await getFiles({ types, searchText, sort });
   return (
     <div className="page-container">
       <section className="w-full">
-        <h1 className="h1 capitalize">{types}</h1>
+        <h1 className="h1 capitalize">{type}</h1>
         <div className="total-size-section">
           <p className="body-1">
             Total : <span className="h-5">0 MB</span>
@@ -20,6 +25,18 @@ const Page = async ({ params }: SearchParamProps) => {
           </div>
         </div>
       </section>
+
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <h1 key={file.$id} className="h1">
+              {file.name}
+            </h1>
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploaded</p>
+      )}
     </div>
   );
 };
--- src/lib/actions/file.actions.ts
+++ src/lib/actions/file.actions.ts
@@ -1,12 +1,13 @@
 "use server";
 
-import { createAdminClient } from "@/lib/appwrite";
+import { createAdminClient, createSessionClient } from "@/lib/appwrite";
 import { InputFile } from "node-appwrite/file";
 import { appwriteConfig } from "@/lib/appwrite/config";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { constructFileUrl, getFileType } from "@/lib/utils";
 import { revalidatePath } from "next/cache";
 import { parseStringify } from "@/lib/helpers";
+import { getCurrentUser } from "@/lib/actions/user.actions";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -60,5 +61,60 @@ export const uploadFile = async ({
     return parseStringify(newFile);
   } catch (error) {
     handleError(error, "Failed to create file document");
+  }
+};
+
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number,
+) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", [currentUser.$id]),
+      Query.contains("users", [currentUser.email]),
+    ]),
+  ];
+
+  /* if (types.length > 0) queries.push(Query.equal("type", types));
+   if (searchText) queries.push(Query.contains("name", searchText));
+  if (limit) queries.push(Query.limit(limit));
+
+  if (sort) {
+    const [sortBy, orderBy] = sort.split("-");
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+    );
+  } */
+
+  return queries;
+};
+
+export const getFiles = async ({
+  types = [],
+  searchText = "",
+  sort = "$createdAt-desc",
+  limit,
+}: GetFilesProps) => {
+  const { databases } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) throw new Error("User not found");
+
+    const queries = createQueries(currentUser, types, searchText, sort, limit);
+
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries,
+    );
+
+    return parseStringify(files);
+  } catch (e) {
+    handleError(e, "Failed to get files");
   }
 };
