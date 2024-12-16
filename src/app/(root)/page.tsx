5d785721d98410dcd07066e571d9d5c3b04f3dbc
rehansyed89 <rehansyed89@gmail.com>
2024-12-16 20:30:00 +0100

    dashboard page done

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { calculateTotalSpaceUsed, getFiles } from "@/lib/actions/file.actions";
-import {
-  cn,
-  convertFileSize,
-  formatDateTime,
-  getUsageSummary,
-} from "@/lib/utils";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
 import { Chart } from "@/components/Chart";
 import Link from "next/link";
 import Image from "next/image";
-import { sum } from "d3-array";
 import { Separator } from "@/components/ui/separator";
 import { FormatDateTime } from "@/components/FormatDateTime";
+import { Models } from "node-appwrite";
+import Thumbnail from "@/components/Thumbnail";
+import { ActionDropdown } from "@/components/ActionDropdown";
 
 const Dashboard = async () => {
   const [files, totalSpace] = await Promise.all([
@@ -56,6 +53,42 @@ const Dashboard = async () => {
             </Link>
           ))}
         </ul>
+      </section>
+
+      <section className="dashboard-recent-files">
+        <h3 className="h3 xl:h2 text-light-100">Recent files uploaded</h3>
+        {files.documents.length > 0 ? (
+          <ul className="mt-5 flex flex-col gap-5">
+            {files.documents.map((file: Models.Document) => (
+              <Link
+                href={file.url}
+                key={file.$id}
+                className="flex items-center gap-3"
+                target="_blank"
+              >
+                <Thumbnail
+                  type={file.type}
+                  extension={file.extension}
+                  url={file.url}
+                />
+                <div className="recent-file-details">
+                  <div className="flex flex-col gap-1">
+                    <p className="recent-file-name">{file.name}</p>
+                    <FormatDateTime
+                      date={file.$createdAt}
+                      className="caption"
+                    />
+                  </div>
+                  <div className="flex flex-col items-end ">
+                    <ActionDropdown file={file} />
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <p className="empty-list"> No files uploaded</p>
+        )}
       </section>
     </div>
   );
